d1c6098903416f6f41ac7deb6e2e345e448a0f47
kjt407 <kjt40700@gmail.com>
2021-09-02 14:27:09 +0900

    cloudmap project init

---
+++ src/main/java/com/example/cloudmap/CloudMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMapApplication.class, args);
+    }
+
+}
