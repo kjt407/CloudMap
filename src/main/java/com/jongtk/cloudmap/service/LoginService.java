e0cfa767232ab4cd4d883834dc0b4e51fe29b910
kjt407 <kjt40700@gmail.com>
2021-09-03 15:33:15 +0900

    commit 2021-09-03

---
+++ src/main/java/com/jongtk/cloudmap/service/LoginService.java
@@ -0,0 +1,44 @@
+package com.jongtk.cloudmap.service;
+
+import com.jongtk.cloudmap.dto.SignupDTO;
+import com.jongtk.cloudmap.entity.Member;
+import com.jongtk.cloudmap.entity.MemberRole;
+import com.jongtk.cloudmap.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public boolean signUp(SignupDTO dto){
+
+        Optional<Member> already =  memberRepository.findById(dto.getEmail());
+
+        if(already.isPresent()) {
+            return false;
+        }
+
+        Member member = Member.builder()
+                .email(dto.getEmail())
+                .name(dto.getName())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .fromSocial(false)
+                .build();
+        member.addMemeberRole(MemberRole.USER);
+
+        memberRepository.save(member);
+
+        return true;
+    }
+}
