8934b313b28483f56f2de6a23fbc6e34ec224327
wnsgudchl0302 <wnsgudchl0302@naver.com>
2021-09-15 12:45:43 +0900

    20210915 1245

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -10,36 +10,40 @@ $(document).ready(function(){
     console.log("asdasdsadas")
     $('.slide-box').toggleClass('active');
   });
+  
 });
 
 
 $(document).on('click','#write-btn',function(){
-  console.log("qqqqq")
-  
-  $('#dialog').dialog({
-    title: '일지 작성하기',
-    modal: true,
-    width: '1000',
-    height: '800',
-    resizeable : false,
-    buttons:{
-      "확인":function(){
-          $(this).dialog("close");
-      },"취소":function(){
-          $(this).dialog("close");
+    $('#dialog').dialog({
+      title: '일지 작성하기',
+      modal: true,
+      width: '1000',
+      height: '800',
+      resizeable : false,
+      open: function(){
+      document.getElementById("jibun-data").innerHTML = document.getElementById("jibun").innerHTML;
+      },
+      buttons:{
+        "확인":function(){
+            $(this).dialog("close");
+        },"취소":function(){
+            $(this).dialog("close");
+        }
+      },
+      background: '#fff',
+      show: {                // 애니메이션 효과 - 보여줄때
+        effect: "blind",
+        duration: 500
+      }, 
+      hide: {                // 애니메이션 효과 - 감출때
+        effect: "blind",
+        duration: 500
       }
-    },
-    background: '#fff',
-    show: {                // 애니메이션 효과 - 보여줄때
-      effect: "blind",
-      duration: 500
-    }, 
-    hide: {                // 애니메이션 효과 - 감출때
-      effect: "blind",
-      duration: 500
-    }
-  });
-
+    });
+  
+ 
+  
   
 });
 
