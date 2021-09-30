2628ddb734ea58d95f59827ad3a806a6f5f1344e
wnsgudchl0302 <wnsgudchl0302@naver.com>
2021-09-30 17:36:39 +0900

    update

--- src/main/resources/static/js/kakaomap.js
+++ src/main/resources/static/js/kakaomap.js
@@ -103,60 +103,48 @@ function searchDetailAddrFromCoords(coords, callback) {
 
 /* 일지 확인하기 */
 /* ************************************************************************ */
-var readContent = document.getElementById('read').innerHTML;
-var positions = [
-    {
-        title: '카카오',
-        content: readContent,
-        latlng: new kakao.maps.LatLng(37.400666, 126.927537)
-    },
-    {
-        title: '생태연못',
-        content: readContent,
-        latlng: new kakao.maps.LatLng(37.398007, 126.940731)
-    },
-    {
-        title: '텃밭',
-        content: readContent,
-        latlng: new kakao.maps.LatLng(37.401023, 126.922025)
-    },
-    {
-        title: '근린공원',
-        content: readContent,
-        latlng: new kakao.maps.LatLng(37.395189, 126.927994)
-    },
-    {
-        title: '근린공원2',
-        content: readContent,
-        latlng: new kakao.maps.LatLng(37.408189, 126.937994)
-    }
-];
+
 var imageListSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 var readMarkerArray = [];
-for (var i = 0; i < positions.length; i++) {
-    // 마커 이미지의 이미지 크기 입니다
-    var imageSize = new kakao.maps.Size(30, 43);
 
-    // 마커 이미지를 생성합니다
-    var markerImage = new kakao.maps.MarkerImage(imageListSrc, imageSize);
+$.get("./getMyList", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        // 마커 이미지의 이미지 크기 입니다
+        var imageSize = new kakao.maps.Size(30, 43);
 
-    var position = positions[i].latlng;
-    // 마커를 생성합니다
-    var markers = new kakao.maps.Marker({
-        map: map, // 마커를 표시할 지도
-        position: position, // 마커를 표시할 위치
-        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        image: markerImage // 마커 이미지
-    });
+        // 마커 이미지를 생성합니다
+        var markerImage = new kakao.maps.MarkerImage(imageListSrc, imageSize);
 
-    var readInfowindow = new kakao.maps.InfoWindow({
-        content: positions[i].content // 인포윈도우에 표시할 내용
-    });
-    readMarkerArray.push(readInfowindow);
 
-    kakao.maps.event.addListener(markers, 'click', makeOverListener(map, markers, readInfowindow));
-    //  kakao.maps.event.addListener(markers, 'mouseout', makeOutListener(infowindow));
-}
+        // 마커를 생성합니다
+        var markers = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: new kakao.maps.LatLng(data[i].lat, data[i].lng), // 마커를 표시할 위치
+            title: data[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image: markerImage // 마커 이미지
+        });
+
+        var titleInfo;
+        if(data[i].title.length>6){
+            titleInfo = data[i].title.substring(0, 6)+"...";
+        }else{
+            titleInfo = data[i].title;
+        }
+
+        document.getElementById("titles").innerHTML = titleInfo;
+
+        var readInfowindow = new kakao.maps.InfoWindow({
+            content: document.getElementById('read').innerHTML // 인포윈도우에 표시할 내용
+        });
+        readMarkerArray.push(readInfowindow);
+
+        kakao.maps.event.addListener(markers, 'click', makeOverListener(map, markers, readInfowindow));
+        //  kakao.maps.event.addListener(markers, 'mouseout', makeOutListener(infowindow));
+
+
+    }
+});
+
 
 
 // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
@@ -411,4 +399,28 @@ $(document).ready(function(){
         }
     });
 
+});
+
+
+// 마커 클러스터러를 생성합니다
+var clusterer = new kakao.maps.MarkerClusterer({
+    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+    minLevel: 6 // 클러스터 할 최소 지도 레벨
+});
+
+// 데이터를 가져오기 위해 jQuery를 사용합니다
+// 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
+$.get("http://localhost:8080/getMyList", function(data) {
+    // 데이터에서 좌표 값을 가지고 마커를 표시합니다
+    // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
+    console.log("112233")
+    var markers = $(data.positions).map(function(i, position) {
+        return new kakao.maps.Marker({
+            position : new kakao.maps.LatLng(position.lat, position.lng)
+        });
+    });
+
+    // 클러스터러에 마커들을 추가합니다
+    clusterer.addMarkers(markers);
 });
